9cdada8b3f07590651cbe540cd4102a97ae9ba95
Minemetero <156386015+Minemetero@users.noreply.github.com>
2024-11-12 20:45:53 +0900

    Update locker3.ts

--- assets/js/locker3.ts
+++ assets/js/locker3.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:assets/js/locker3.ts
 const flag : string = "020dd3dc162a25483a5648c8ea007dc379ed5cb922f5cc7e4d2c48b3b69cc354"; // this cannot be decoded hahahahahhahahahahahahhahahhaahahaha
 
 const hashData = async (data : string) => {
@@ -14,8 +13,6 @@ console.log(flag);
 
 // look into the images closely, there may be something hidden in them :)
 
-// hashData("THERE_IS_ALWAYS_A_PATH_TO_SUCCESS").then(hash => console.log(hash));
-=======
 const flag : string = "020dd3dc162a25483a5648c8ea007dc379ed5cb922f5cc7e4d2c48b3b69cc354"; // this cannot be decoded hahahahahhahahahahahahhahahhaahahaha
 
 const hashData = async (data : string) => {
@@ -30,4 +27,3 @@ const checkTrue = (data : string) => {
 console.log(flag);
 
 // look into the images closely, there may be something hidden in them :)
->>>>>>> abe2ef6b740e6d7c9f2b664e812244f7a89e1c10:locker3/scripts/script.ts
