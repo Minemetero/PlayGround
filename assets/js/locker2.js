7d6a780a384078c59122f3a694f6b9655a192a63
Minemetero <minemetero@outlook.com>
2024-11-13 23:04:52 +0900

    style: Spaces...

--- assets/js/locker2.js
+++ assets/js/locker2.js
@@ -1,49 +1,49 @@
-        // Function to hash the flag using SHA-256
-        async function hashFlag(message) {
-            const encoder = new TextEncoder();
-            const data = encoder.encode(message);
-            const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-            const hashArray = Array.from(new Uint8Array(hashBuffer));
-            return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-        }
+// Function to hash the flag using SHA-256
+async function hashFlag(message) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+}
 
-        // Pre-hashed correct flag
-        const correctFlagHash = 'b1fc7cae5c24f2625866f490e53b17f4e4ddb30c33d6422ff650dce5a8345253';
+// Pre-hashed correct flag
+const correctFlagHash = 'b1fc7cae5c24f2625866f490e53b17f4e4ddb30c33d6422ff650dce5a8345253';
 
-        async function checkFlag() {
-            const enteredFlag = document.getElementById('flagInput').value;
-            const hashedEnteredFlag = await hashFlag(enteredFlag);
-            const successMessage = "Q29uZ3JhdHVsYXRpb25zISBZb3UndmUgdW5sb2NrZWQgdGhlIGxvY2tlci4gTXIuQyBpcyB2ZXJ5IGN1bm5pbmcsIGhlIGhhcyBhbHJlYWR5IHRyYW5zZmVycmVkIHRoZSBzZWNyZXQgYmVmb3JlIHlvdXIgYXJyaXZhbA==";
+async function checkFlag() {
+    const enteredFlag = document.getElementById('flagInput').value;
+    const hashedEnteredFlag = await hashFlag(enteredFlag);
+    const successMessage = "Q29uZ3JhdHVsYXRpb25zISBZb3UndmUgdW5sb2NrZWQgdGhlIGxvY2tlci4gTXIuQyBpcyB2ZXJ5IGN1bm5pbmcsIGhlIGhhcyBhbHJlYWR5IHRyYW5zZmVycmVkIHRoZSBzZWNyZXQgYmVmb3JlIHlvdXIgYXJyaXZhbA==";
 
-            if (hashedEnteredFlag === correctFlagHash) {
-                document.querySelector('.locker').textContent = '🔓';
-                alert(atob(successMessage));
-                // Redirect to the harder game
-                window.location.href = 'locker3.html';
-            } else {
-                alert("Incorrect flag! Keep trying!");
-            }
-        }
+    if (hashedEnteredFlag === correctFlagHash) {
+        document.querySelector('.locker').textContent = '🔓';
+        alert(atob(successMessage));
+        // Redirect to the harder game
+        window.location.href = 'locker3.html';
+    } else {
+        alert("Incorrect flag! Keep trying!");
+    }
+}
 
-        function checkRiddleAnswer() {
-            const answer = document.getElementById('riddleInput').value.toLowerCase();
-            if (answer === 'echo') {//I know you don't know the answer to this riddle because I don't know this riddle either
-                const riddleClueEncoded = 'UGFydCBvZiB0aGUgZmxhZyBpczogVEhFX09OTFlf'; 
-                document.getElementById('riddleClue').textContent = atob(riddleClueEncoded);
-                document.getElementById('riddleClue').classList.remove('hidden');
-            } else {
-                alert("Incorrect! Try again.");
-            }
-        }
+function checkRiddleAnswer() {
+    const answer = document.getElementById('riddleInput').value.toLowerCase();
+    if (answer === 'echo') {//I know you don't know the answer to this riddle because I don't know this riddle either
+        const riddleClueEncoded = 'UGFydCBvZiB0aGUgZmxhZyBpczogVEhFX09OTFlf';
+        document.getElementById('riddleClue').textContent = atob(riddleClueEncoded);
+        document.getElementById('riddleClue').classList.remove('hidden');
+    } else {
+        alert("Incorrect! Try again.");
+    }
+}
 
-        function checkCipherAnswer() {
-            const answer = document.getElementById('cipherInput').value.toUpperCase();
-            const encodedCorrectAnswer = 'VEhFUkUgSVMgTk8gU0VDUkVU'; 
-            if (answer === atob(encodedCorrectAnswer)) {
-                const cipherClueEncoded = 'U0lOR0xFX0dFTklVUw=='; 
-                document.getElementById('cipherClue').textContent = atob(cipherClueEncoded);
-                document.getElementById('cipherClue').classList.remove('hidden');
-            } else {
-                alert("Incorrect! Try again.");
-            }
-        }
+function checkCipherAnswer() {
+    const answer = document.getElementById('cipherInput').value.toUpperCase();
+    const encodedCorrectAnswer = 'VEhFUkUgSVMgTk8gU0VDUkVU';
+    if (answer === atob(encodedCorrectAnswer)) {
+        const cipherClueEncoded = 'U0lOR0xFX0dFTklVUw==';
+        document.getElementById('cipherClue').textContent = atob(cipherClueEncoded);
+        document.getElementById('cipherClue').classList.remove('hidden');
+    } else {
+        alert("Incorrect! Try again.");
+    }
+}
--- assets/js/locker3.js
+++ assets/js/locker3.js
@@ -9,8 +9,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function () { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -39,43 +39,47 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 var flag = "020dd3dc162a25483a5648c8ea007dc379ed5cb922f5cc7e4d2c48b3b69cc354"; // this cannot be decoded hahahahahhahahahahahahhahahhaahahaha
 var valueOf2;
 
-var hashData = function (data) { return __awaiter(void 0, void 0, void 0, function () {
-    var encodedData, hashBuffer;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                encodedData = new TextEncoder().encode(data);
-                return [4 /*yield*/, crypto.subtle.digest("SHA-256", encodedData)];
-            case 1:
-                hashBuffer = _a.sent();
-                return [2 /*return*/, Array.prototype.slice.call(new Uint8Array(hashBuffer)).map(function (b) { return b.toString(16).padStart(2, '0'); }).join('')];
-        }
+var hashData = function (data) {
+    return __awaiter(void 0, void 0, void 0, function () {
+        var encodedData, hashBuffer;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    encodedData = new TextEncoder().encode(data);
+                    return [4 /*yield*/, crypto.subtle.digest("SHA-256", encodedData)];
+                case 1:
+                    hashBuffer = _a.sent();
+                    return [2 /*return*/, Array.prototype.slice.call(new Uint8Array(hashBuffer)).map(function (b) { return b.toString(16).padStart(2, '0'); }).join('')];
+            }
+        });
     });
-}); };
+};
 
-var checkTrue = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var valueOf, _a;
-    return __generator(this, function (_b) {
-        switch (_b.label) {
-            case 0:
-                valueOf = document.getElementById("password");
-                valueOf2 = (valueOf === null || valueOf === void 0 ? void 0 : valueOf.value) || null;
-                if (valueOf == null) {
+var checkTrue = function () {
+    return __awaiter(void 0, void 0, void 0, function () {
+        var valueOf, _a;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    valueOf = document.getElementById("password");
+                    valueOf2 = (valueOf === null || valueOf === void 0 ? void 0 : valueOf.value) || null;
+                    if (valueOf == null) {
+                        return [2 /*return*/];
+                    }
+                    ;
+                    _a = flag;
+                    return [4 /*yield*/, hashData(valueOf2 === null || valueOf2 === void 0 ? void 0 : valueOf2.toUpperCase())];
+                case 1:
+                    if (_a === (_b.sent())) {
+                        alert("Password is correct");
+                        window.location.replace('https://tinyurl.com/3sykfcnt');
+                        return [2 /*return*/];
+                    }
+                    alert("Password is incorrect");
                     return [2 /*return*/];
-                }
-                ;
-                _a = flag;
-                return [4 /*yield*/, hashData(valueOf2 === null || valueOf2 === void 0 ? void 0 : valueOf2.toUpperCase())];
-            case 1:
-                if (_a === (_b.sent())) {
-                    alert("Password is correct");
-                    window.location.replace('https://tinyurl.com/3sykfcnt');
-                    return [2 /*return*/];
-                }
-                alert("Password is incorrect");
-                return [2 /*return*/];
-        }
+            }
+        });
     });
-}); };
+};
 // look into the images closely, there may be something hidden in them :)
 console.log(flag);
